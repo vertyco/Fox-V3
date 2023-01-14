615ecddd70c22ade66351bde55091624e0b9bdf2
Alex <alex.c.goble@gmail.com>
2023-01-14 10:29:20 -0500

    fix for dpy2

--- fifo/__init__.py
+++ fifo/__init__.py
@@ -4,8 +4,7 @@ from .fifo import FIFO
 
 # Applying fix from: https://github.com/Azure/azure-functions-python-worker/issues/640
 # [Fix] Create a wrapper for importing imgres
-from .date_trigger import *
-from . import CustomDateTrigger
+from .date_trigger import CustomDateTrigger
 
 # [Fix] Register imgres into system modules
 sys.modules["CustomDateTrigger"] = CustomDateTrigger
@@ -13,7 +12,7 @@ sys.modules["CustomDateTrigger"] = CustomDateTrigger
 
 async def setup(bot):
     cog = FIFO(bot)
-    bot.add_cog(cog)
+    await bot.add_cog(cog)
     await cog.initialize()
 
 
